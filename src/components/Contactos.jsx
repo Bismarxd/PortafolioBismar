31c2b8adcc78e16e2e9674f98cb8289afa77213e
Bismarxd <bismarckmaytatintaya@gmail.com>
2023-06-13 21:17:41 -0400

    deployment

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -5,28 +5,32 @@ const Contactos = () => {
     <div className=" h-[30rem]">
       <h1 className="text-center shadow-sm shadow-white text-slate-400 text-4xl m-5 p-5 uppercase">Contacto</h1>
 
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+      <form className="z-10 mx-auto max-w-4xl text-center m-10 h-[126px]" name="contacto" netifly>
+        {/* input, submit y textarea son clases css */}
+        <div className="flex gap-4">
+          <div className="w-1/2">          
+            <input className="input" type="text" id="nombre" name="nombre" placeholder="Nombre"/>
+            <input className="input" type="email" id="email" name="email" placeholder="Correo Electronico"/>
+            <input className="input" type="number" id="numero" name="celular" placeholder="Numero de Celular"/>
+          </div>
 
+          <div className="w-1/2">
+            <textArea
+              className="textarea h-[12rem]"
+              id="mensaje"
+              name="mensaje"
+              placeholder="Mensaje"
+            ></textArea>
+          </div>
 
+        </div>
+        
+        <button 
+          className="submit w-full mt-5 mb-5 p-4 rounded-xl text-white bg-slate-950 text-2xl uppercase"
+          type="submit" 
+          id="enviar"
+        >Enviar</button>
+      </form>
     </div>
   )
 }
