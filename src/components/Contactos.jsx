b4f8e43e107c975eb2d7e3227af43ddd93ada13c
Bismarxd <bismarckmaytatintaya@gmail.com>
2023-06-13 21:13:34 -0400

    deployment

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -5,12 +5,28 @@ const Contactos = () => {
     <div className=" h-[30rem]">
       <h1 className="text-center shadow-sm shadow-white text-slate-400 text-4xl m-5 p-5 uppercase">Contacto</h1>
 
-      <form name="contacto" method="POST" data-netlify="true" action="/gracias">
-  <input type="text" name="nombre" placeholder="Nombre" />
-  <input type="email" name="correo" placeholder="Correo electrónico" />
-  <textarea name="mensaje" placeholder="Mensaje"></textarea>
-  <button type="submit" className="bg-white">Enviar</button>
+      <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
 </form>
+
+
     </div>
   )
 }
